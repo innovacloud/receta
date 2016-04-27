31cb6a9d9ef908c78154a5b6a19b95914a61fed4
Ramses Tovar <ramses.tovar@gmail.com>
2016-04-27 02:49:34 -0430

    Test show Recipe method in front-end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # To handle the case of a non-existent recipe, send http 404
+  rescue_from ActiveRecord::RecordNotFound do
+    respond_to do |type|
+      type.all  { render :nothing => true, :status => 404 }
+    end
+  end
+
 end
